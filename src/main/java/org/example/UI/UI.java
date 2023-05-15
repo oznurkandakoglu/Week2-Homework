fc668d983774e6bbf22e5228a79d1850cd3adcb3
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2023-05-16 00:54:54 +0300

    added comment line

--- src/main/java/org/example/UI/UI.java
+++ src/main/java/org/example/UI/UI.java
@@ -20,68 +20,80 @@ public class UI {
         Scanner input = new Scanner(System.in);
         int selected;
         selected = input.nextInt();
-    while(true){
-        switch (selected){
+        // Created select menu
+        while(true){
+            switch (selected){
 
-            case 1:
-                System.out.println("-HOUSE LIST-");
-                printCase(estateService, estateList, "House");
+                // Print house properties
+                case 1:
+                    System.out.println("-HOUSE LIST-");
+                    printCase(estateService, estateList, "House");
 
-                break;
-            case 2:
-                System.out.println("-SUMMERHOUSE LIST-");
-                printCase(estateService, estateList, "SummerHouse");
-                break;
-            case 3:
-                System.out.println("-VILLA LIST-");
-                printCase(estateService, estateList, "Villa");
+                    break;
 
-                break;
-            case 4:
+                // Print summerhouse properties
+                case 2:
+                    System.out.println("-SUMMERHOUSE LIST-");
+                    printCase(estateService, estateList, "SummerHouse");
+                    break;
 
-                System.out.println("-----------------------------------------------------");
-                System.out.println("Total price of all houses: $" + estateService.getTotalPrice());
-                System.out.println("Average square meters of all houses: " + estateService.getAverageArea());
-                System.out.println("-----------------------------------------------------");
+                // Print villa properties
+                case 3:
+                    System.out.println("-VILLA LIST-");
+                    printCase(estateService, estateList, "Villa");
 
-                break;
-            case 5:
+                    break;
 
-                Scanner count = new Scanner(System.in);
-                System.out.println("-----------------------------------------------------");
-                System.out.println("Select room count: ");
-                int roomCount = count.nextInt();
-                System.out.println("Select living room count: ");
-                int livingRoomCount = count.nextInt();
-                List<Estate> filteredHouses = estateService.filterByRoomAndLivingRoom(estateList,roomCount, livingRoomCount);
-                System.out.println("Houses with " + roomCount + " rooms and " + livingRoomCount + " living rooms:");
-                for (Estate estate : filteredHouses) {
-                    System.out.println(estate.getType() + " : $" + estate.getPrice() + " - " + estate.getArea() + " square meters");
-                }
-                System.out.println("-----------------------------------------------------");
+                // Print all house properties
+                case 4:
 
-                break;
-            default :
-                System.out.println("Please select:" +
-                        "\n1-House List" +
-                        "\n2-Summerhouse List" +
-                        "\n3-Villa List" +
-                        "\n4-All Houses Details" +
-                        "\n5-Filter House ");
-                selected = input.nextInt();
-                break;
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("Total price of all houses: $" + estateService.getTotalPrice());
+                    System.out.println("Average square meters of all houses: " + estateService.getAverageArea());
+                    System.out.println("-----------------------------------------------------");
+
+                    break;
+
+                // Print filtered house
+                case 5:
+
+                    Scanner count = new Scanner(System.in);
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("Select room count: ");
+                    int roomCount = count.nextInt();
+                    System.out.println("Select living room count: ");
+                    int livingRoomCount = count.nextInt();
+                    List<Estate> filteredHouses = estateService.filterByRoomAndLivingRoom(estateList,roomCount, livingRoomCount);
+                    System.out.println("Houses with " + roomCount + " rooms and " + livingRoomCount + " living rooms:");
+                    for (Estate estate : filteredHouses) {
+                        System.out.println(estate.getType() + " : $" + estate.getPrice() + " - " + estate.getArea() + " square meters");
+                    }
+                    System.out.println("-----------------------------------------------------");
+
+                    break;
+                default :
+                    System.out.println("Please select:" +
+                            "\n1-House List" +
+                            "\n2-Summerhouse List" +
+                            "\n3-Villa List" +
+                            "\n4-All Houses Details" +
+                            "\n5-Filter House ");
+                    selected = input.nextInt();
+                    break;
+            }
+            System.out.println("Please select:" +
+                    "\n1-House List" +
+                    "\n2-Summerhouse List" +
+                    "\n3-Villa List" +
+                    "\n4-All Houses Details" +
+                    "\n5-Filter House ");
+
+            selected = input.nextInt();
         }
-        System.out.println("Please select:" +
-                "\n1-House List" +
-                "\n2-Summerhouse List" +
-                "\n3-Villa List" +
-                "\n4-All Houses Details" +
-                "\n5-Filter House ");
-
-        selected = input.nextInt();
-    }
 
     }
+
+    // Created print function for estate types
     public void printCase(EstateService estateService, List<Estate> estateList, String type){
         for (Estate estate : estateService.getListByType(estateList, type)) {
 
